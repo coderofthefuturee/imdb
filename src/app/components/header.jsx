72817fa865a0e1324e8c90584d3d13b46906b4d3
Prince <princeking950@gmail.com>
2023-09-06 18:52:47 -0700

    DarkMode

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -3,18 +3,19 @@ import MenuItem from './menuitem';
 import Link from "next/link";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModeSwitch from './DarkModeSwitch.jsx';
 
 export default function header() {
   return (
-    <div className='flex justify-between mx-2 max-w-6x1 sm:mx-auto items-center py-6'>
-        <div className="flex" >
+    <div className='flex justify-between mx-2 max-w-6x1 sm:mx-auto items-center py-3'>
+        <div className="flex">
             < MenuItem title="Home" address="/" Icon={AiFillHome}/>
             < MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill}/>
         </div>
-        <div className="flex" >
+        <div className="flex items-center space-x-5" >
             <Link href="/">
                 <h2 className='text-2xl'><span className='font-bold text-black bg-amber-500 py-1 px-2 rounded-lg mr-1'>IMDb</span>
-                <span className='text-xl hidden sm:inline-flex'>Clone</span></h2>
+                <span className='text-xl hidden sm:inline-flex px-3'>Clone</span></h2>
             </Link>
         </div>
     </div>
--- src/app/components/menuitem.jsx
+++ src/app/components/menuitem.jsx
@@ -4,7 +4,7 @@ export default function MenuItem({title, address, Icon}) {
   return (
     <div>
         <Link href={address} className="flex mx-4 lg:mx-6 hover:text-amber-600">
-            < Icon className="text-2x1 mx-4"/>
+            <Icon className="text-2xl mx-4 move-down-5" />
             <p className="hidden sm:inline my-2 text-sm">{title}</p>
         </Link>
     </div>
