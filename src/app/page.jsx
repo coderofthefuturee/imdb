be21b1dd44fead3eb9e816f1a5d1a98512587f94
Prince <princeking950@gmail.com>
2023-09-08 21:06:56 -0700

    Creat the about page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <h1 
-    className='text-red-500'>Home
-    </h1>
-  )
+  // return (
+    // <h1 
+    // className='text-red-500'>Home
+    // </h1>
+  // )
 }
 
 
