64a32eea6c71f6314a7e146ca8e9f41f279e3b93
Prince <princeking950@gmail.com>
2023-09-11 17:18:10 -0700

    Fetch data from tmdb api in the home page

--- src/app/components/NavbarItem.jsx
+++ src/app/components/NavbarItem.jsx
@@ -11,7 +11,7 @@ export default function NavbarItem({ title, param }) {
     <div className={`flex font-semibold w-full text-gray-700 justify-center dark:bg-amber-500 bg-amber-100 lg:text-lg p-3 hover:underline hover:text-black bold-underline ${
         genre &&
         genre === param &&
-        "underline underline-offset-8 decoration-4  decoration-black rounded-lg"
+        "underline underline-offset-8 decoration-4  decoration-amber-500 rounded-lg"
     }`}>
         <Link href={`/?genre=${param}`}>
             {title}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,31 @@
-import Image from 'next/image'
+export const dynamic = "force-dynamic"; // this is the fix
+const fetch = require('node-fetch');
+import Results from "./components/Results";
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
-  // return (
-  //   <h1 
-  //   className='text-red-500'>Home
-  //   </h1>
-  // )
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch'); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
 
 
@@ -17,11 +37,15 @@ export default function Home() {
 
 
 
+// import Image from 'next/image'
 
-
-
-
-
+// export default function Home() {
+//   // return (
+//   //   <h1 
+//   //   className='text-red-500'>Home
+//   //   </h1>
+//   // )
+// }
 
 
 
