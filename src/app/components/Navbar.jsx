402f4131e09d72b45e825a38c5ed009959d40fa2
Prince <princeking950@gmail.com>
2023-09-21 13:10:29 -0700

    note

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from './NavbarItem';
 export default function Navbar() {
   return (
     <div className='flex justify-center space-x-5 px-5'>
-      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Trending (Educational Purposes)" param="fetchTrending" />
       <NavbarItem title="Top Rated" param="fetchTopRated" />
     </div>
   );
